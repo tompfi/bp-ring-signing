204aa90f12ded9f906a94522c02acd33be8b1ee7
tom_pf <tom.pfirsig@gmail.com>
2025-10-08 15:47:38 +0200

    Delete ring-signature-bp directory

--- ring-signature-bp/django/ringSignatureBP/signing/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SigningConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'signing'
--- ring-signature-bp/django/ringSignatureBP/signing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-
-from . import views
-
-urlpatterns = [
-    path("", views.sign, name="sign"),
-    path('verifier/', views.verifier, name='verifier'),
-    path('github-lookup/', views.github_lookup, name='github_lookup'),
-]
